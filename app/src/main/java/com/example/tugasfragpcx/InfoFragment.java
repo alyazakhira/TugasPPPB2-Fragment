910a9fc10023cbb023b39241ee6aab4d05c4ab5f
alyazakhira <101548825+alyazakhira@users.noreply.github.com>
2023-02-26 10:48:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasfragpcx/InfoFragment.java
@@ -0,0 +1,34 @@
+package com.example.tugasfragpcx;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment extends Fragment {
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+    public static InfoFragment newInstance() {
+        return new InfoFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
