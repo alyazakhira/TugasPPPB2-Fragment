9ea0b333f3a51cac78e452424ab9044aec3dfd8e
alyazakhira <101548825+alyazakhira@users.noreply.github.com>
2023-02-26 12:07:09 +0700

    second commit

---
+++ app/src/main/java/com/example/tugasfragpcx/StrFragment.java
@@ -0,0 +1,35 @@
+package com.example.tugasfragpcx;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StrFragment extends Fragment {
+
+    public StrFragment() {
+        // Required empty public constructor
+    }
+
+    public static StrFragment newInstance() {
+        return new StrFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_str, container, false);
+    }
+}
